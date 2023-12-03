48e7ba4106ec37d185e3116a78de081d4a92bd4c
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-03 16:01:01 +0400

    Add files via upload

---
+++ FactoryDesignPattern_task/FactoryDesignPattern_task/Button.cs
@@ -0,0 +1,33 @@
+﻿namespace FactoryDesignPattern_task;
+
+public interface Button
+{
+    public void render();
+    public void onClick();
+}
+
+public class WindowsButton : Button
+{
+    public void onClick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void render()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class HTMLButton : Button
+{
+    public void onClick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void render()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FactoryDesignPattern_task/FactoryDesignPattern_task/Dialog.cs
@@ -0,0 +1,34 @@
+﻿namespace FactoryDesignPattern_task;
+
+public class Dialog
+{
+    public Dialog() { }
+
+    public void render()
+    {
+        Button okButton = createButton();
+        okButton.onClick();
+        okButton.render();
+    }
+
+    public static Button createButton()
+    {
+        return createButton();
+    }
+}
+
+public class WindowsDialog : Dialog
+{
+    public Button createButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+public class WebDialog : Dialog
+{
+    public Button createButton()
+    {
+        return createButton();
+    }
+}
